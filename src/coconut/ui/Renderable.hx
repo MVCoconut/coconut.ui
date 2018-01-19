0f9b9ab13d475779a4c0318599bad4d9740084bb
Juraj Kirchheim <back2dos@googlemail.com>
2018-01-19 14:01:36 +0100

    Add shouldViewUpdate lifecycle method.

---
+++ src/coconut/ui/Renderable.hx
@@ -0,0 +1,5 @@
+package coconut.ui;
+
+interface Renderable {
+  function getRenderResult():RenderResult;
+}
--- src/coconut/ui/View.hx
+++ src/coconut/ui/View.hx
@@ -2,16 +2,33 @@ package coconut.ui;
 
 import tink.state.Observable;
 
+using tink.CoreApi;
+
 @:autoBuild(coconut.ui.macros.ViewBuilder.build())
-class View extends Renderer {
+@:observable
+class View extends Renderer implements Renderable {
   
   @:noCompletion var __coco__cache = new coconut.ui.tools.ViewCache();
   
   @:keep public var id(default, null):Int = idCounter++; static var idCounter = 0;
 
   public function new(render:Void->coconut.ui.RenderResult, ?pos:haxe.PosInfos) {
-    super(Observable.auto(function () { 
-      return __coco__cache.cached(render);
+    var last:Option<RenderResult> = None;
+    super(Observable.auto(function () return { 
+      if (!shouldViewUpdate() && last != None) {
+        trace('skip $this');
+        last.force();
+      }
+      else {
+        var res = __coco__cache.cached(render);
+        last = Some(res);
+        res;
+      }
     }));
   }
+
+  @:noCompletion function shouldViewUpdate():Bool return true;
+
+  @:noCompletion inline public function getRenderResult():RenderResult 
+    return this;
 }
