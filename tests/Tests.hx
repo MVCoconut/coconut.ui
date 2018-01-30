c4b7d54c7937e3eda4d04e7cf533419c7ed6b253
Juraj Kirchheim <back2dos@googlemail.com>
2018-01-30 23:08:44 +0100

    Add test for #4.

--- tests/Tests.hx
+++ tests/Tests.hx
@@ -317,3 +317,18 @@ class FooBar {
   public function foo() {}
   public function bar() {}
 }
+
+class MyView extends View {
+  function render() '
+    <switch ${int()}>
+      <case ${0}>
+        <div>Zero</div>
+      <case ${1}>
+        <div>One</div>
+      <case ${_}>
+        <div>Default</div>
+    </switch>
+  ';
+  
+  function int() return 1;
+}
