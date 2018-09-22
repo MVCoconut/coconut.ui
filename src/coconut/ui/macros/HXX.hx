8b9af794bf1f8be43da71c530763557bfb4eda12
Juraj Kirchheim <back2dos@googlemail.com>
2018-09-22 20:22:01 +0200

    Wire up void tag support.

--- src/coconut/ui/macros/HXX.hx
+++ src/coconut/ui/macros/HXX.hx
@@ -8,8 +8,15 @@ using tink.MacroApi;
 
 class HXX {
   static public var generator = new Generator();
-  static public function parse(e:Expr) 
-    return generator.root(
-      tink.hxx.Parser.parseRoot(e, { defaultExtension: 'hxx', noControlStructures: false, defaultSwitchTarget: macro __data__ })
+  static public function parse(e:Expr) {
+    var ctx = generator.createContext();
+    return ctx.generateRoot(
+      tink.hxx.Parser.parseRoot(e, { 
+        defaultExtension: 'hxx', 
+        noControlStructures: false, 
+        defaultSwitchTarget: macro __data__,
+        isVoid: ctx.isVoid
+      })
     );
+  }
 }
