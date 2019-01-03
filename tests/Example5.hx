48d8e4b1676e8f18768d20271e3d9332c73cb0e4
Juraj Kirchheim <back2dos@googlemail.com>
2019-01-03 10:17:37 +0100

    Add test for #35.

--- tests/Example5.hx
+++ tests/Example5.hx
@@ -3,9 +3,10 @@ import coconut.ui.*;
 class Example5<T> extends View {
   @:attribute var data:T;
   @:attribute var renderer:{ data: T }->Children;
-  function render() '
+  @:computed var content:RenderResult = hxx('
     <div class="example5">
       {...renderer({ data: data })}
     </div>
-  ';
+  ');
+  function render() return content;
 }
