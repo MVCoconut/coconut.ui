aef6d877f5e777134bebaf9a4041524f2ba7e9b9
Juraj Kirchheim <back2dos@googlemail.com>
2019-02-05 05:14:00 +0100

    Allow adding defaults via priority queue.

--- src/coconut/ui/macros/HXX.hx
+++ src/coconut/ui/macros/HXX.hx
@@ -6,9 +6,15 @@ package coconut.ui.macros;
 import haxe.macro.*;
 import tink.hxx.*;
 using tink.MacroApi;
+using tink.CoreApi;
 
 class HXX {
-  static public var generator = new Generator();
+  
+  static public var defaults(default, never) = new tink.priority.Queue<Lazy<Array<Named<Tag>>>>();
+
+  static public var generator = new Generator(function () 
+    return [for (group in defaults) for (tag in group.get()) tag]
+  );
   static public function parse(e:Expr) {
     var ctx = generator.createContext();
     return ctx.generateRoot(
