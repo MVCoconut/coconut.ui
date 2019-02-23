f96cf33803f66e912beef7fbcf7c2b2200da03f7
Juraj Kirchheim <back2dos@googlemail.com>
2019-02-23 10:15:23 +0100

    Update to match latest hxx.

--- src/coconut/ui/macros/HXX.hx
+++ src/coconut/ui/macros/HXX.hx
@@ -10,7 +10,7 @@ using tink.CoreApi;
 
 class HXX {
   
-  static public var defaults(default, never) = new tink.priority.Queue<Lazy<Array<Named<Tag>>>>();
+  static public var defaults(default, never) = new tink.priority.Queue<Lazy<Array<Named<Expr.Position->Tag>>>>();
 
   static public var generator = new Generator(function () 
     return [for (group in defaults) for (tag in group.get()) tag]
