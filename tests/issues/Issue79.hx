cd52df4838608e02ad593bf4a5eb222bb5673308
Kevin Leung <kevinresol@gmail.com>
2021-03-04 15:12:08 +0800

    Ensure the type of @:loaded var

--- tests/issues/Issue79.hx
+++ tests/issues/Issue79.hx
@@ -4,5 +4,11 @@ import tink.CoreApi;
 
 class Issue79 extends View {
   @:loaded var foo:String = Promise.resolve('foo');
-  function render() '<div />';
+  function render() '
+    <switch ${foo}>
+      <case ${Loading}>Loading
+      <case ${Done(v)}>$v
+      <case ${Failed(e)}>${e.toString()}
+    </switch>
+  ';
 }
