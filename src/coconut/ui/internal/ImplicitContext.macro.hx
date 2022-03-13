3023d91540da8470dabf12791ca5383bf5c19343
Juraj Kirchheim <juraj.kirchheim@doclerholding.com>
2022-03-13 11:51:51 +0100

    Unwrap parens for implicit context.

--- src/coconut/ui/internal/ImplicitContext.macro.hx
+++ src/coconut/ui/internal/ImplicitContext.macro.hx
@@ -6,11 +6,15 @@ using tink.MacroApi;
 class ImplicitContext {
 
  static function with(e:Expr) {
+    switch e {
+      case macro ($v): e = v;
+      default:
+    }
     var exprs = switch e.expr { case EArrayDecl(a): a; default: [e]; };
     var entries = [for (e in exprs) switch e {
       case macro $k => $v:
         macro @:pos(e.pos) new coconut.ui.internal.ImplicitContext.SingleImplicit($k, function () return $v);
-      default: e.reject('expected key => value');
+      default: e.reject('expected key => value, but got ${e.toString()}');
     }];
     return macro new coconut.ui.internal.ImplicitContext.ImplicitValues([$a{entries}]);
   }
